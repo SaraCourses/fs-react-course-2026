c9f8e9e303f1aff308ab80caf281201fff005d52
Web Courses <sara.web.courses@gmail.com>
2026-02-20 09:18:52 +0200

    add description to about page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,5 +1,8 @@
 const AboutPage = () => {
-    return <h1>AboutPage</h1>
+    return <div>
+        <h1>AboutPage</h1>
+        <h2>This is about page of react course</h2>
+    </div>
 }
 
 export default AboutPage 
