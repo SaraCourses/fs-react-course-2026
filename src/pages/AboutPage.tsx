101c15b20ec315fd1ddaac720eaf7b09d7d9650c
Web Courses <sara.web.courses@gmail.com>
2026-02-20 09:33:16 +0200

    add h3

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,6 +2,7 @@ const AboutPage = () => {
     return <div>
         <h1>AboutPage</h1>
         <h2>This is about page of react course</h2>
+        <h3>H3</h3>
         <button>Click</button>
     </div>
 }
